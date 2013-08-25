5d28919370fbd8cccf20bae36ecbbfb3baa24bc9
Dave Rapin <dave@rapin.com>
2013-08-25 16:55:39 -0400

    refactoring

--- assets/javascripts/scoreboard/directives.js
+++ assets/javascripts/scoreboard/directives.js
@@ -1,12 +1,21 @@
 /* Directives */
 
-scoreboardApp.directive("gameState", function() {
+scoreboardApp.directive("scoreboardGameState", function() {
   return function(scope, element, attrs) {
     // If the game is over, we no longer show the start time.
     if (scope.game.state == "FINAL") {
       element.text(scope.game.state);
     } else {
       element.text(scope.game.startsAt);
+    }
+  }
+});
+
+scoreboardApp.directive("scoreboardGameWinning", function() {
+  return function(scope, element, attrs) {
+    // If the game is over, we no longer show the start time.
+    if (scope.team.winning) {
+      element.addClass("scoreboard-game-line-winning");
     }
   }
 });
